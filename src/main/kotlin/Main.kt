78aae959d415322ac5fbb88e04940bb99c43b4f9
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-31 06:00:38 +0900

    Chapter3 - Converting classic HTML renderファンクション内でroot内にタグを記述

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,46 @@
 import kotlinx.browser.document
+import react.dom.attrs
+import react.dom.div
+import react.dom.h1
+import react.dom.h3
+import react.dom.img
+import react.dom.p
+import react.dom.render
 
 fun main() {
-    document.bgColor = "blue"
+    render(document.getElementById("root")) {
+        h1 {
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
 }
