9a4643966fc131c505fa119d883276226ec3dca4
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-31 06:25:22 +0900

    Chapter3 - Writing typesafe CSS - styled-componentsを利用してVideo Playerを画面右上の配置

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 import kotlinx.browser.document
+import kotlinx.css.Position
+import kotlinx.css.position
+import kotlinx.css.px
+import kotlinx.css.right
+import kotlinx.css.top
 import react.dom.attrs
 import react.dom.div
 import react.dom.h1
@@ -6,6 +11,18 @@ import react.dom.h3
 import react.dom.img
 import react.dom.p
 import react.dom.render
+import styled.css
+import styled.styledDiv
+
+val unwatchedVideos = listOf<KotlinVideo>(
+    KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf<KotlinVideo>(
+    KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
 
 fun main() {
 
@@ -27,7 +44,12 @@ fun main() {
             }
             watchedVideos.forEach { video -> p { +"${video.speaker}: ${video.title}" } }
         }
-        div {
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
             h3 {
                 +"John Doe: Building and breaking things"
             }
@@ -39,13 +61,3 @@ fun main() {
         }
     }
 }
-
-val unwatchedVideos = listOf<KotlinVideo>(
-    KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-    KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-    KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-)
-
-val watchedVideos = listOf<KotlinVideo>(
-    KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-)
