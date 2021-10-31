f9150cd1ccf724baa06f64bca1896851b6bb0155
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-31 10:23:29 +0900

    Chapter6: Adding a button and wiring it - VideoPlayerコンポーネントにVideoを視聴済/未視聴に切り替えるボタンを追加 - ここではボタンのデザインのみ作成していて押した時の処理は未実装

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -1,8 +1,13 @@
+import kotlinx.css.Color
+import kotlinx.css.Display
 import kotlinx.css.Position
+import kotlinx.css.backgroundColor
+import kotlinx.css.display
 import kotlinx.css.position
 import kotlinx.css.px
 import kotlinx.css.right
 import kotlinx.css.top
+import kotlinx.html.js.onClickFunction
 import react.Props
 import react.RBuilder
 import react.RComponent
@@ -11,10 +16,13 @@ import react.dom.attrs
 import react.dom.h3
 import react.dom.img
 import styled.css
+import styled.styledButton
 import styled.styledDiv
 
 external interface VideoPlayerProps : Props {
     var video: Video
+    var onWatchedButtonPressed: (Video) -> Unit
+    var unwatchedVideo: Boolean
 }
 
 class VideoPlayer : RComponent<VideoPlayerProps, State>() {
@@ -27,6 +35,18 @@ class VideoPlayer : RComponent<VideoPlayerProps, State>() {
             }
             h3 {
                 +"${props.video.speaker}: ${props.video.title}"
+            }
+            styledButton {
+                css {
+                    display = Display.block
+                    backgroundColor = if (props.unwatchedVideo) Color.lightGreen else Color.red
+                }
+                attrs {
+                    onClickFunction = {
+                        props.onWatchedButtonPressed(props.video)
+                    }
+                }
+                if (props.unwatchedVideo) +"Mark as watched" else +"Mark as unwatched"
             }
             img {
                 attrs {
